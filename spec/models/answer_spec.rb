d13ac89e1ed145bb131725fde7b692c3175c984a
ilya <ilya89forrails@gmail.com>
2017-02-11 19:32:30 +0900

    spec fixes

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,8 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
-  it { is_expected.to belong_to(:question) }
+  it { is_expected.to belong_to(:question)}
 
   it { should validate_presence_of :body }
-  it { should validate_presence_of :question_id }
 end
